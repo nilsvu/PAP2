123cec14d67b99992523cf51ddeb7a0583650a25
Christian Kohlstedde <christian@kohlsted.de>
2014-02-18 19:12:05 +0100

    Error fixing

--- papstats.py
+++ papstats.py
@@ -28,12 +28,12 @@ def pformat(v, dv, label, signi=2, unit=None):
         o = 10**(e-signi+1)
         v = round_ordnung(v, o)
         dv = round_ordnung(dv, o)
-        return (r"$%s=(%."+str(signi-1)+"f\pm%."+str(signi-1)+"f)*10^{%d}%s$") % (label, v / 10**e, dv / 10**e, e, unit)
+        return (r"$%s(%."+str(signi-1)+"f\pm%."+str(signi-1)+"f)*10^{%d}%s$") % (label, v / 10**e, dv / 10**e, e, unit)
     else:
         e = get_e(v)
         o = 10**(e-signi+1)
         v = round_ordnung(v, o)
-        string = r"$%s=%.1f*10^{%d}%s$"
+        string = r"$%s%."+str(signi-1)+"f*10^{%d}%s$"
         return string % (label, v/10.0**e, e,  unit)
 
 def round_ordnung(v, o):

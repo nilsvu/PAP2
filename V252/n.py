6192613b8a2666391a636159c2ded10a70377032
Nils Fischer <n.fischer@viwid.com>
2014-02-24 20:53:35 +0100

    added V252 file

---
+++ V252/n.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Auswertung des Versuchs 252 - Aktivierung von Indium und von Silber mit thermischen Neutronen
+Physikalisches Anfängerpraktikum II, Universität Heidelberg
+
+Author: Nils Fischer
+'''
+
+import numpy as np
+import uncertainties as unc
+import uncertainties.unumpy as unp
+import matplotlib.pyplot as plt
+import scipy.constants as const

730d780ef452140ff1b5a2ba699c1627dbd5a18a
Nils Fischer <n.fischer@viwid.com>
2014-03-06 22:26:47 +0100

    completed V256

--- V256/n.py
+++ V256/n.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 '''
-    Auswertung des Versuchs 255 - Röntgenfluoreszenz
-    Physikalisches Anfängerpraktikum II, Universität Heidelberg
-    
-    Author: Nils Fischer
-    '''
+Auswertung des Versuchs 256 - Röntgenfluoreszenz
+Physikalisches Anfängerpraktikum II, Universität Heidelberg
+
+Author: Nils Fischer
+'''
 
 import numpy as np
 import scipy as sp
@@ -59,7 +59,7 @@ plt.title(u'Diagramm 3.1: Röntgenfluoreszenzspektrum verschiedener Elemente')
 plt.xlabel('Strahlungsenergie $E \, [keV]$')
 plt.ylabel(u'Zählrate '+r'$n \, [\frac{Ereignisse}{s}]$')
 for i in range(data.shape[0]):
-    p = papstats.plot_data(data[i,:,0], data[i,:,1], label=elements[i][0], elinewidth=0.5, capsize=4)
+    p = papstats.plot_data(data[i,:,0], data[i,:,1], label=elements[i][0]+' ('+str(elements[i][1])+')', elinewidth=0.5, capsize=4)
     plt.fill_between(data[i,:,0], 0, unp.nominal_values(data[i,:,1]), color=p[0].get_color(), alpha=0.1)
 #plt.xlim(np.min(data[:,:,0]), np.max(data[:,:,0]))
 #plt.ylim(np.min(unp.nominal_values(data[:,:,1])), np.max(unp.nominal_values(data[:,:,1])))
@@ -85,12 +85,14 @@ s = Z[:, np.newaxis]-ERy/np.sqrt(n1**(-2)-n2**(-2))
 
 # Table output
 table = pt.PrettyTable()
-table.add_column('Elemente', elements)
+table.add_column('Element', [e[0]+' ('+str(e[1])+')' for e in elements])
 for k in range(np.shape(E)[1]):
-    table.add_column('E'+str(k+1), E[:,k])
-    table.add_column('(E'+str(k+1)+'/Ry)^1/2', ERy[:,k])
-    table.add_column('sigma'+str(k+1), s[:,k])
-#print table
+    l = ['alpha', 'beta'][k]
+    table.add_column('E_'+l+' [keV]', [('{:.2u}'.format(e) if e.s!=0 else '{:.2f}'.format(e.n)) for e in E[:,k]/const.kilo/const.eV])
+    table.add_column('(E_'+l+'/Ry)^1/2', [('{:.2u}'.format(e) if e.s!=0 else '{:.2f}'.format(e.n)) for e in ERy[:,k]])
+    table.add_column('sigma_'+l, [('{:.2u}'.format(e) if e.s!=0 else '{:.2f}'.format(e.n)) for e in s[:,k]])
+with open('Resources/3.1.txt', 'w') as file:
+    file.write(table.get_string())
 
 # Plot
 plt.clf()
@@ -113,7 +115,8 @@ ax3.set_xticklabels(np.array(elements)[:,0][np.argsort(Z)])
 for k in range(np.shape(E)[1]):
     l = 'K_'+[r'\alpha', r'\beta'][k]
     p = papstats.plot_data(Z, ERy[:,k], label='$'+l+'$ Messpunkte', ax=ax1)
-    ax1.plot(xspace, moseley(xspace, s=k+1, n1=1, n2=k+2), ls='dotted', label='$'+l+'$ nach Moseley\'schem Gesetz', color=p[0].get_color())
+    pstats = papstats.PAPStats(ydata=unp.nominal_values(ERy[:,k][unp.std_devs(ERy[:,k])>0]), ymodel=moseley(Z[unp.std_devs(ERy[:,k])>0], s=k+1, n1=1, n2=k+2), sigma=unp.std_devs(ERy[:,k])[unp.std_devs(ERy[:,k])>0], ddof=2)
+    ax1.plot(xspace, moseley(xspace, s=k+1, n1=1, n2=k+2), ls='dotted', label='$'+l+'$ nach Moseley\'schem Gesetz\n'+pstats.legendstring(), color=p[0].get_color())
     papstats.plot_data(Z, s[:,k], ax=ax2, color=p[0].get_color())
     ax2.axhline(k+1, color=p[0].get_color())
     ax2.fill_between(np.sort(Z), k+1, unp.nominal_values(s[:,k])[np.argsort(Z)], alpha=0.15, color=p[0].get_color())
@@ -129,10 +132,11 @@ papstats.savefig_a4('3.2.png')
 
 converters = dict.fromkeys(range(1), unc.ufloat_fromstr)
 data = np.array([np.loadtxt('legierungen/'+l+'.txt', converters=converters, dtype=object) for l in legierungen])*const.kilo*const.eV
+table_strings = []
 for i in range(data.shape[0]):
     E_L = data[i]
     table = pt.PrettyTable()
-    table.add_column(u'Mögliche Legierung '+legierungen[i]+':', ['Abweichung [keV]', 'Sigma'])
+    table.add_column(u'Moegliche Legierung '+legierungen[i]+':', ['Abweichung [keV]', 'Sigma'])
     containing_elements = []
     for j in range(E.shape[0]):
         diffs = np.abs([E[j] - E_l for E_l in E_L])/const.kilo/const.eV
@@ -142,15 +146,6 @@ for i in range(data.shape[0]):
         if sigma <=1:
             containing_elements.append((j, sigma))
             table.add_column(elements[j][0],  np.array(papstats.rdiff(diff_min))[[0,2]])
-
-#    print "Mögliche Legierung:", " + ".join([elements[e[0]][0]+" ("+str(e[1])+")" for e in containing_elements])
-    print table
-
-
-
-
-
-
-
-
-
+    table_strings.append(table.get_string())
+with open('Resources/3.2.txt', 'w') as file:
+    file.write('\n'.join(table_strings))

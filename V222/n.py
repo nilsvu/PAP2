0eff51e40148fe2b2ca71cc21419d642634f7126
Nils Fischer <n.fischer@viwid.com>
2014-04-29 15:19:25 +0200

    added V222 directory

---
+++ V222/n.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+'''
+Auswertung des Versuchs 256 - Röntgenfluoreszenz
+Physikalisches Anfängerpraktikum II, Universität Heidelberg
+
+Author: Nils Fischer
+'''
+
+import numpy as np

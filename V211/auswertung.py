c1a9b1c350bfc8df7cb95e53fb74922820640de9
Nils Fischer <n.fischer@viwid.com>
2014-07-01 16:00:28 +0200

    v211 auswertung completed

--- V211/auswertung.py
+++ V211/auswertung.py
@@ -20,55 +20,177 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir)
 import papstats
 
-def macheSchoenenSchwingungsGraphen(file, xmin, xmax):
-    data = np.loadtxt(file + '.txt', skiprows=1)
-
-    title = open(file + '.txt').readline()
-
-    plt.clf()
-
-    x  = data[:, 0]
-    y1 = data[:, 1]
-    y2 = data[:, 2]
-
-    xrange = (x >= xmin) & (x <= xmax)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)    # The big subplot
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212, sharex=ax1)
-
-    # Turn off axis lines and ticks of the big subplot
-    ax.spines['top'].set_color('none')
-    ax.spines['bottom'].set_color('none')
-    ax.spines['left'].set_color('none')
-    ax.spines['right'].set_color('none')
-    ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
-
-    ax1.plot(x[xrange], y1[xrange])
-    ax2.plot(x[xrange], y2[xrange])
-
-    ax1.yaxis.set_label_position("right")
-    ax2.yaxis.set_label_position("right")
-    ax1.set_ylabel(u'Pendel 1')
-    ax2.set_ylabel(u'Pendel 2')
-
-    ax.set_title(title)
-    ax.set_xlabel(ur'Zeit $t$ in $s$')
-    ax.set_ylabel(ur'Auslenkung in $^\circ \, (Grad)$')
-
-    ax1.set_xlim([xmin, xmax])
-
-    papstats.savefig_a4(file + '.png')
+# Unicode Hack
+import sys
+import codecs
+#sys.stdout=codecs.getwriter('utf-8')(sys.stdout)
 
 
-macheSchoenenSchwingungsGraphen('3.1.1', 0, 10)
-macheSchoenenSchwingungsGraphen('3.1.2', 0, 10)
-macheSchoenenSchwingungsGraphen('3.2.1', 0, 10)
-macheSchoenenSchwingungsGraphen('3.2.2', 0, 10)
-macheSchoenenSchwingungsGraphen('3.3.1', 0, 10)
-macheSchoenenSchwingungsGraphen('3.3.2', 0, 10)
+# Functions
 
-macheSchoenenSchwingungsGraphen('4.1', 0, 50)
-macheSchoenenSchwingungsGraphen('4.2', 0, 30)
-macheSchoenenSchwingungsGraphen('4.3', 0, 15)
+def print_w(T1, T2):
+	print papstats.pformat(T1, unit='s', label='T_1')
+	print papstats.pformat(T2, unit='s', label='T_2')
+	w1 = 2*const.pi/T1
+	w2 = 2*const.pi/T2
+	w = np.mean([w1, w2])
+	print papstats.pformat(w1, unit='Hz', label='w_1')
+	print papstats.pformat(w2, unit='Hz', label='w_2')
+	print papstats.pformat(w, unit='Hz', label='w')
+	return (w1, w2, w)
+
+
+def schwingung_table(l, t1, t2, n):
+	T1, T2 = t1 / n, t2 / n
+	T = np.mean([T1, T2], axis=0)
+	w = 2 * const.pi / T
+	return (papstats.table(labels=['Kopplung', 'T_1', 'T_2', 'T', 'w'], units=[None, 's', 's', 's', 'Hz'], columns=[papstats.pformat(l / const.centi, label='l', unit='cm', format='c'), T1, T2, T, w]), w)
+
+
+def vergleich_table(l, w, w_erw):
+	diff = w - w_erw
+	return papstats.table(labels=['Kopplung', 'w', 'w_erw', 'w-w_erw', 'Sigmabereich'], units=[None, 'Hz', 'Hz', 'Hz', None], columns=[papstats.pformat(l / const.centi, label='l', unit='cm', format='c'), w, w_erw, diff, unp.nominal_values(np.abs(diff))/unp.std_devs(diff)])
+
+
+# Konstanten
+
+dt = 0.05
+
+
+print '\n# 2.1 Offsets'
+
+data = np.loadtxt('offset.txt', skiprows=1)
+offset1 = np.mean(data[:,1])
+offset2 = np.mean(data[:,2])
+print papstats.pformat(offset1, label='Offset phi_1', unit='grad')
+print papstats.pformat(offset2, label='Offset phi_2', unit='grad')
+
+
+print "\n# 2.2 Eigenfrequenzen der ungekoppelten Pendel"
+
+t1 = unc.ufloat(25.51, dt) - unc.ufloat(1.27, dt)
+t2 = unc.ufloat(25.17, dt) - unc.ufloat(0.88, dt)
+n = 15
+T1, T2 = t1 / n, t2 / n
+print_w(T1, T2)
+
+
+print "\n# 2.3 Gekoppelte Pendel"
+
+data_symm = []
+data_anti = []
+
+l = unc.ufloat(19.2, 0.1) * const.centi
+t1 = unc.ufloat(33.08, dt) - unc.ufloat(0.77, dt)
+t2 = unc.ufloat(33.06, dt) - unc.ufloat(0.82, dt)
+n = 20
+data_symm.append([l, t1, t2, n])
+
+t1 = unc.ufloat(32.19, dt) - unc.ufloat(1.17, dt)
+t2 = unc.ufloat(31.42, dt) - unc.ufloat(0.44, dt)
+n = 20
+data_anti.append([l, t1, t2, n])
+
+l = unc.ufloat(0.292, 0.001)
+t1 = unc.ufloat(32.50, dt) - unc.ufloat(0.24, dt)
+t2 = unc.ufloat(32.48, dt) - unc.ufloat(0.23, dt)
+n = 20
+data_symm.append([l, t1, t2, n])
+
+t1 = unc.ufloat(33.10, dt) - unc.ufloat(0.77, dt)
+t2 = unc.ufloat(33.84, dt) - unc.ufloat(1.48, dt)
+n = 22
+data_anti.append([l, t1, t2, n])
+
+l = unc.ufloat(0.392, 0.001)
+t1 = unc.ufloat(33.88, dt) - unc.ufloat(1.67, dt)
+t2 = unc.ufloat(33.89, dt) - unc.ufloat(1.65, dt)
+n = 20
+data_symm.append([l, t1, t2, n])
+
+t1 = unc.ufloat(28.36, dt) - unc.ufloat(1.05, dt)
+t2 = unc.ufloat(29.03, dt) - unc.ufloat(1.73, dt)
+n = 20
+data_anti.append([l, t1, t2, n])
+
+print "Symmetrische Schwingung:"
+(table, w_1) = schwingung_table(*np.transpose(data_symm))
+print table
+print "Antisymmetrische Schwingung:"
+(table, w_2) = schwingung_table(*np.transpose(data_anti))
+print table
+
+
+print "\n# 2.4 Superposition der Anregung"
+
+data_schwing = []
+data_schweb = []
+
+l = unc.ufloat(0.392, 0.001)
+t1 = unc.ufloat(29.30, dt) - unc.ufloat(1.87, dt)
+t2 = unc.ufloat(34.10, dt) - unc.ufloat(6.68, dt)
+n = 20
+data_schwing.append([l, t1, t2, n])
+
+dt_Schwebung = 0.2
+t1 = unc.ufloat(29.30, dt_Schwebung) - unc.ufloat(1.87, dt_Schwebung)
+t2 = unc.ufloat(34.10, dt_Schwebung) - unc.ufloat(6.68, dt_Schwebung)
+n = 1.5
+data_schweb.append([l, t1, t2, n])
+
+l = unc.ufloat(0.292, 0.001)
+t1 = unc.ufloat(39.11, dt) - unc.ufloat(2.20, dt)
+t2 = unc.ufloat(50.27, dt) - unc.ufloat(13.34, dt)
+n = 25
+data_schwing.append([l, t1, t2, n])
+
+dt_Schwebung = 0.5
+t1 = unc.ufloat(64.92, dt_Schwebung) - unc.ufloat(14.06, dt_Schwebung)
+t2 = unc.ufloat(56.25, dt_Schwebung) - unc.ufloat(5.39, dt_Schwebung)
+n = 1.5
+data_schweb.append([l, t1, t2, n])
+
+l = unc.ufloat(0.192, 0.001)
+t1 = unc.ufloat(26.00, dt) - unc.ufloat(2.29, dt)
+t2 = unc.ufloat(45.34, dt) - unc.ufloat(21.61, dt)
+n = 15
+data_schwing.append([l, t1, t2, n])
+
+dt_Schwebung = 0.5
+t1 = unc.ufloat(76.52, dt_Schwebung) - unc.ufloat(36.71, dt_Schwebung)
+t2 = unc.ufloat(56.63, dt_Schwebung) - unc.ufloat(17.00, dt_Schwebung)
+n = 0.5
+data_schweb.append([l, t1, t2, n])
+
+data_schwing = np.array(data_schwing)
+data_schwing = data_schwing[np.argsort(data_schwing[:,0])]
+data_schweb = np.array(data_schweb)
+data_schweb = data_schweb[np.argsort(data_schweb[:,0])]
+
+l = data_schwing[:,0]
+
+print "Schwingung:"
+(table, w_I) = schwingung_table(*np.transpose(data_schwing))
+print table
+w_I_erw = (w_1 + w_2) / 2.
+print vergleich_table(l, w_I, w_I_erw)
+print "Schwebung:"
+(table, w_II) = schwingung_table(*np.transpose(data_schweb))
+print table
+w_II_erw = (w_2 - w_1) / 2.
+print vergleich_table(l, w_II, w_II_erw)
+
+
+print u"\n# Kopplungsgrade"
+
+V_l = l**2 / np.roll(l, 1)**2
+K = (w_2**2-w_1**2)/2/w_1**2
+print K
+V_w = K / np.roll(K, 1)
+diff = V_l-V_w
+K_labels = ['K_1', 'K_2', 'K_3']
+K_labels = [K_labels[i] + '/' + np.roll(K_labels, 1)[i] for i in range(len(K_labels))]
+print papstats.table(labels=['', 'V_l', 'V_w', 'V_l-V_w', 'Sigmabereich'], columns=[K_labels, V_l, V_w, diff, unp.nominal_values(np.abs(diff))/unp.std_devs(diff)])
+
+
+
--- V211/durchfuehrung.py
+++ V211/durchfuehrung.py
@@ -20,233 +20,63 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir)
 import papstats
 
-# Unicode Hack
-import sys
-import codecs
-sys.stdout=codecs.getwriter('utf-8')(sys.stdout)
+def macheSchoenenSchwingungsGraphen(filename, xmin, xmax):
+    data = np.loadtxt(filename + '.txt', skiprows=2)
 
+    with open(filename + '.txt') as file:
+        title = file.readline()
+        try:
+            l = unc.ufloat(float(file.readline()), 0.1)*const.centi
+            title = title + 'mit Kopplung bei $' + papstats.pformat(l / const.centi, label='l', unit='cm', format='l') + '$'
+        except ValueError, e:
+            pass
 
-print '### 2.1 Offsets'
-data = np.loadtxt('1.offset.txt', skiprows=1)
+    plt.clf()
 
-offset1 = np.mean(data[:,1])
-offset2 = np.mean(data[:,2])
+    x  = data[:, 0]
+    y1 = data[:, 1]
+    y2 = data[:, 2]
 
-print u'Offset des Pendel 1', offset1
-print u'Offset des Pendel 2', offset2
+    xrange = (x >= xmin) & (x <= xmax)
 
-print ''
-print "### 2.2 Eigenfrequenzen der ungekoppelten Pendel ###"
+    fig = plt.figure()
+    ax = fig.add_subplot(111) # The big subplot
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212, sharex=ax1)
 
-t1 = unc.ufloat(25.51, 0.05) - unc.ufloat(1.27, 0.05)
-t2 = unc.ufloat(25.17, 0.05) - unc.ufloat(0.88, 0.05)
+    # Turn off axis lines and ticks of the big subplot
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.spines['right'].set_color('none')
+    ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
 
-n = 15
+    ax1.plot(x[xrange], y1[xrange])
+    ax2.plot(x[xrange], y2[xrange])
 
-T1, T2 = t1 / n, t2 / n
+    ax1.yaxis.set_label_position("right")
+    ax2.yaxis.set_label_position("right")
+    ax1.set_ylabel(u'Pendel 1')
+    ax2.set_ylabel(u'Pendel 2')
 
-print papstats.pformat(t1, unit='s', label='t_1')
-print papstats.pformat(t2, unit='s', label='t_2')
-print papstats.pformat(T1, unit='s', label='T_1')
-print papstats.pformat(T2, unit='s', label='T_2')
+    ax.set_title('Diagramm 2.' + filename + ': ' + title)
+    ax.set_xlabel(ur'Zeit $t \, [s]$')
+    ax.set_ylabel(ur'Auslenkung $\phi \, [^\circ]$')
 
-print ''
-print "### 2.3 Gekoppelte Pendel ###"
+    ax1.set_xlim([xmin, xmax])
 
-l = unc.ufloat(0.192, 0.001)
-print papstats.pformat(l, unit='m', label=u'Länge l')
+    papstats.savefig_a4(filename + '.png')
 
-print "symmetrische Eigenschwingungen"
-t1 = unc.ufloat(33.08, 0.05) - unc.ufloat(0.77, 0.05)
-t2 = unc.ufloat(33.06, 0.05) - unc.ufloat(0.82, 0.05)
 
-n = 20
+macheSchoenenSchwingungsGraphen('1', 0, 10)
 
-T1, T2 = t1 / n, t2 / n
+macheSchoenenSchwingungsGraphen('2.1', 0, 10)
+macheSchoenenSchwingungsGraphen('2.2', 0, 10)
+macheSchoenenSchwingungsGraphen('2.3', 0, 10)
+macheSchoenenSchwingungsGraphen('2.4', 0, 10)
+macheSchoenenSchwingungsGraphen('2.5', 0, 10)
+macheSchoenenSchwingungsGraphen('2.6', 0, 10)
 
-print papstats.pformat(t1, unit='s', label='t_1')
-print papstats.pformat(t2, unit='s', label='t_2')
-print papstats.pformat(T1, unit='s', label='T_1')
-print papstats.pformat(T2, unit='s', label='T_2')
-
-
-print "antisymmetrische Eigenschwingungen"
-t1 = unc.ufloat(32.19, 0.05) - unc.ufloat(1.17, 0.05)
-t2 = unc.ufloat(31.42, 0.05) - unc.ufloat(0.44, 0.05)
-
-n = 20
-
-T1, T2 = t1 / n, t2 / n
-
-print papstats.pformat(t1, unit='s', label='t_1')
-print papstats.pformat(t2, unit='s', label='t_2')
-print papstats.pformat(T1, unit='s', label='T_1')
-print papstats.pformat(T2, unit='s', label='T_2')
-
-print ''
-
-l = unc.ufloat(0.292, 0.001)
-print papstats.pformat(l, unit='m', label=u'Länge l')
-
-
-print "symmetrische Eigenschwingungen"
-t1 = unc.ufloat(32.50, 0.05) - unc.ufloat(0.24, 0.05)
-t2 = unc.ufloat(32.48, 0.05) - unc.ufloat(0.23, 0.05)
-
-n = 20
-
-T1, T2 = t1 / n, t2 / n
-
-print papstats.pformat(t1, unit='s', label='t_1')
-print papstats.pformat(t2, unit='s', label='t_2')
-print papstats.pformat(T1, unit='s', label='T_1')
-print papstats.pformat(T2, unit='s', label='T_2')
-
-print "antisymmetrische Eigenschwingungen"
-t1 = unc.ufloat(33.10, 0.05) - unc.ufloat(0.77, 0.05)
-t2 = unc.ufloat(33.84, 0.05) - unc.ufloat(1.48, 0.05)
-
-n = 22
-
-T1, T2 = t1 / n, t2 / n
-
-print papstats.pformat(t1, unit='s', label='t_1')
-print papstats.pformat(t2, unit='s', label='t_2')
-print papstats.pformat(T1, unit='s', label='T_1')
-print papstats.pformat(T2, unit='s', label='T_2')
-
-print ''
-
-l = unc.ufloat(0.392, 0.001)
-print papstats.pformat(l, unit='m', label=u'Länge l')
-
-print "symmetrische Eigenschwingungen"
-t1 = unc.ufloat(33.88, 0.05) - unc.ufloat(1.67, 0.05)
-t2 = unc.ufloat(33.89, 0.05) - unc.ufloat(1.65, 0.05)
-
-n = 20
-
-T1, T2 = t1 / n, t2 / n
-
-print papstats.pformat(t1, unit='s', label='t_1')
-print papstats.pformat(t2, unit='s', label='t_2')
-print papstats.pformat(T1, unit='s', label='T_1')
-print papstats.pformat(T2, unit='s', label='T_2')
-
-print "antisymmetrische Eigenschwingungen"
-t1 = unc.ufloat(28.36, 0.05) - unc.ufloat(1.05, 0.05)
-t2 = unc.ufloat(29.03, 0.05) - unc.ufloat(1.73, 0.05)
-
-n = 20
-
-T1, T2 = t1 / n, t2 / n
-
-print papstats.pformat(t1, unit='s', label='t_1')
-print papstats.pformat(t2, unit='s', label='t_2')
-print papstats.pformat(T1, unit='s', label='T_1')
-print papstats.pformat(T2, unit='s', label='T_2')
-
-print ''
-print "### 2.4 Superposition der Anrengung ###"
-
-print ''
-print "## Messung 1"
-
-l = unc.ufloat(0.392, 0.001)
-print u"Länge", papstats.pformat(l)
-
-print "Schwingungsperiodendauer"
-dt_Schwingung = 0.05
-t1_1 = unc.ufloat(29.30, dt_Schwingung) - unc.ufloat(1.87, dt_Schwingung)
-t2_1 = unc.ufloat(34.10, dt_Schwingung) - unc.ufloat(6.68, dt_Schwingung)
-
-n = 20
-
-T1_1, T2_1 = t1_1 / n, t2_1 / n
-
-print papstats.pformat(t1_1, unit='s', label='t1_1')
-print papstats.pformat(t2_1, unit='s', label='t2_1')
-print papstats.pformat(T1_1, unit='s', label='T1_1')
-print papstats.pformat(T2_1, unit='s', label='T2_1')
-
-print "Schwebungsperiodendauer"
-dt_Schwebung = 0.2
-t1_2 = unc.ufloat(29.30, dt_Schwebung) - unc.ufloat(1.87, dt_Schwebung)
-t2_2 = unc.ufloat(34.10, dt_Schwebung) - unc.ufloat(6.68, dt_Schwebung)
-
-n = 1.5
-
-T1_2, T2_2 = t1_2 / n, t2_2 / n
-
-print papstats.pformat(t1_2, unit='s', label='t1_2')
-print papstats.pformat(t2_2, unit='s', label='t2_2')
-print papstats.pformat(T1_2, unit='s', label='T1_2')
-print papstats.pformat(T2_2, unit='s', label='T2_2')
-
-print ''
-print "## Messung 2"
-
-l = unc.ufloat(0.292, 0.001)
-print u"Länge", papstats.pformat(l)
-
-print "Schwingungsperiodendauer"
-dt_Schwingung = 0.05
-t1_1 = unc.ufloat(39.11, dt_Schwingung) - unc.ufloat(2.20, dt_Schwingung)
-t2_1 = unc.ufloat(50.27, dt_Schwingung) - unc.ufloat(13.34, dt_Schwingung)
-
-n = 25
-
-T1_1, T2_1 = t1_1 / n, t2_1 / n
-
-print papstats.pformat(t1_1, unit='s', label='t1_1')
-print papstats.pformat(t2_1, unit='s', label='t2_1')
-print papstats.pformat(T1_1, unit='s', label='T1_1')
-print papstats.pformat(T2_1, unit='s', label='T2_1')
-
-print "Schwebungsperiodendauer"
-dt_Schwebung = 0.5
-t1_2 = unc.ufloat(64.92, dt_Schwebung) - unc.ufloat(14.06, dt_Schwebung)
-t2_2 = unc.ufloat(56.25, dt_Schwebung) - unc.ufloat(5.39, dt_Schwebung)
-
-n = 1.5
-
-T1_2, T2_2 = t1_2 / n, t2_2 / n
-
-print papstats.pformat(t1_2, unit='s', label='t1_2')
-print papstats.pformat(t2_2, unit='s', label='t2_2')
-print papstats.pformat(T1_2, unit='s', label='T1_2')
-print papstats.pformat(T2_2, unit='s', label='T2_2')
-
-print ''
-print "## Messung 3"
-
-l = unc.ufloat(0.192, 0.001)
-print u"Länge", papstats.pformat(l)
-
-print "Schwingungsperiodendauer"
-dt_Schwingung = 0.05
-t1_1 = unc.ufloat(26.00, dt_Schwingung) - unc.ufloat(2.29, dt_Schwingung)
-t2_1 = unc.ufloat(45.34, dt_Schwingung) - unc.ufloat(21.61, dt_Schwingung)
-
-n = 15
-
-T1_1, T2_1 = t1_1 / n, t2_1 / n
-
-print papstats.pformat(t1_1, unit='s', label='t1_1')
-print papstats.pformat(t2_1, unit='s', label='t2_1')
-print papstats.pformat(T1_1, unit='s', label='T1_1')
-print papstats.pformat(T2_1, unit='s', label='T2_1')
-
-print "Schwebungsperiodendauer"
-dt_Schwebung = 0.5
-t1_2 = unc.ufloat(76.52, dt_Schwebung) - unc.ufloat(36.71, dt_Schwebung)
-t2_2 = unc.ufloat(56.63, dt_Schwebung) - unc.ufloat(17.00, dt_Schwebung)
-
-n = 0.5
-
-T1_2, T2_2 = t1_2 / n, t2_2 / n
-
-print papstats.pformat(t1_2, unit='s', label='t1_2')
-print papstats.pformat(t2_2, unit='s', label='t2_2')
-print papstats.pformat(T1_2, unit='s', label='T1_2')
-print papstats.pformat(T2_2, unit='s', label='T2_2')
+macheSchoenenSchwingungsGraphen('3.1', 0, 50)
+macheSchoenenSchwingungsGraphen('3.2', 0, 30)
+macheSchoenenSchwingungsGraphen('3.3', 0, 15)

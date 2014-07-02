960243acc1dfb960af3ffe748a0d924bb793094d
Nils Fischer <n.fischer@viwid.com>
2014-07-03 01:44:42 +0200

    completed v212

--- V212/auswertung.py
+++ V212/auswertung.py
@@ -35,11 +35,12 @@ rho_F = unc.ufloat(1.1442, 0.0002) * const.gram/const.centi**3
 drho = rho_K - rho_F
 
 t = np.reshape(t, (len(r), 5))
-t = unp.uarray(np.mean(t, axis=1), np.std(t, axis=1))
+t = unp.uarray(np.mean(t, axis=1), np.std(t, axis=1)/np.sqrt(len(t)))
 
 v = s / t
 v_k = v / drho
-v_kl = v_k * (1 + 2.1 * r / R)
+y = (1 + 2.1 * r / R)
+v_kl = v_k * y
 r_sq = r**2
 
 def fit_linear_origin(x, m):
@@ -53,38 +54,40 @@ print papstats.pformat(eta, label='eta')
 v_lam = 2. / 9. * const.g * drho / eta * r_sq
 
 plt.clf()
+plt.title(u'Diagramm 3.1: Bestimmung der Viskosität nach Stokes')
 papstats.plot_data(r_sq / const.centi**2, v_k, label='Messwerte')
 papstats.plot_data(r_sq / const.centi**2, v_kl, label='Ladenburgkorrigierte Messwerte', color='red')
 papstats.plot_data(r_sq / const.centi**2, v_lam / drho, label='Erwartungswerte', color='orange')
-papstats.plot_fit(fit_linear_origin, popt, pstats, xspace=unp.nominal_values(r_sq), xscale=1./const.centi**2, eq=r'\frac{v}{\rho_K-\rho_F}=m*r^2')
-papstats.plot_fit(fit_linear_origin, popt_l, pstats_l, xspace=unp.nominal_values(r_sq), xscale=1./const.centi**2, eq=r'\frac{v}{\rho_K-\rho_F}=m*r^2')
+papstats.plot_fit(fit_linear_origin, popt, pstats, xspace=unp.nominal_values(r_sq), xscale=1./const.centi**2, eq=r'\frac{v}{\rho_K-\rho_F}=m*r^2', punits=[r'\frac{m^2}{kg*s}'])
+papstats.plot_fit(fit_linear_origin, popt_l, pstats_l, xspace=unp.nominal_values(r_sq), xscale=1./const.centi**2, eq=r'\frac{v}{\rho_K-\rho_F}=m*r^2', punits=[r'\frac{m^2}{kg*s}'])
 plt.xlabel('$r^2 \, [cm^2]$ mit $r$: Kugelradius')
 plt.ylabel(r'$\frac{v}{\rho_K-\rho_F}$ mit $v$: mittlere Sinkgeschwindigkeit')
 plt.legend(loc='upper left')
 papstats.savefig_a4('1.png')
 
-print papstats.table(labels=['v', 'v_k', 'v_lam'], columns=[v, v_kl, v_lam])
+V = v / v_lam
+Re = rho_F * v * 2. * r / eta
 
+plt.clf()
+plt.title(u'Diagramm 3.2: Abschätzung der kritischen Reynoldszahl')
+plt.xscale('log')
+papstats.plot_data(Re, V, color='black')
+plt.xlabel('$Re$')
+plt.ylabel(r'$\frac{v}{v_{lam}}$')
+papstats.savefig_a4('2.png')
+
+print papstats.table(labels=['d=2r', 'r^2', 'v', 'v/(rho_K-rho_F)', u'λ', 'v_k', 'v_k/(rho_K-rho_F)', 'v_lam', 'v/v_lam', 'Re'], units=['mm', 'mm^2', 'mm/s', 'mm^4/(g*s)', None, 'mm/s', 'mm^4/(g*s)', 'mm/s', None, None], columns=[2 * r / const.milli, r_sq / const.milli**2, v / const.milli, v_k / const.milli**4 * const.gram, y, v * y / const.milli, v_kl / const.milli**4 * const.gram, v_lam / const.milli, V, Re])
 
 #####
 print u"#2: Bestimmung der Viskosität nach Hagen-Poiseuille"
 #####
 
-Vt = unc.ufloat(25, 0.2) * const.centi**3 / (9 * 60 + 39.08)
+Vt = unc.ufloat(25, 0.2 * np.sqrt(2)) * const.centi**3 / (9 * 60 + 39.08)
 L = unc.ufloat(100, 0.5) * const.milli
 R = unc.ufloat(1.5, 0.01) / 2. * const.milli
+h_A, h_E = 536, 530.5
 
-V = v / v_lam
-Re = rho_F * v * 2. * r / eta
-
-plt.clf()
-plt.xscale('log')
-papstats.plot_data(Re, V)
-plt.xlabel('$Re$')
-plt.ylabel(r'$\frac{v}{v_{lam}}$')
-papstats.savefig_a4('2.png')
-
-dp = rho_F * const.g * np.mean([unc.ufloat(536, 1), unc.ufloat(530.5, 1)]) * const.milli
+dp = rho_F * const.g * unc.ufloat(h_A + h_E, h_A - h_E) / 2. * const.milli
 print dp, R, Vt, L
 
 eta_2 = const.pi * dp * R**4 / 8. / Vt / L

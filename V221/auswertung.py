e2a0ab42cb345d410c0c71ee65c3e4df161f3738
Nils Fischer <n.fischer@viwid.com>
2014-07-06 01:21:16 +0200

    completed v221

--- V221/auswertung.py
+++ V221/auswertung.py
@@ -18,23 +18,40 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir)
 import papstats
 
+
+# Konstanten
+k_L_erw = 0.78 * 1.401 + 0.21 * 1.398 + 0.01 * 1.648
+k_Ar_erw = 1.648
+print k_L_erw, k_Ar_erw
+
+#####
+print u"# Methode nach Clement und Desormes"
+#####
+
 data = np.loadtxt('2.1.txt', skiprows=1)
-
-d_h = [0.2 for i in range(len(data[:,0]))]
-
+d_h = 0.2
 h1 = unp.uarray(data[:,0], d_h) - unp.uarray(data[:,1], d_h)
-
 h3 = unp.uarray(data[:,2], d_h) - unp.uarray(data[:,3], d_h)
-
 k = h1 / (h1 - h3)
 
+print papstats.table(labels=['k'], columns=[k])
+
+k = np.mean(k)
+print papstats.pformat(k, label='k')
+papstats.print_rdiff(k, k_L_erw)
+
+
+#####
+print u"# Methode nach Rüchhardt"
+#####
 
 p_0 = unc.ufloat(1004.6, 0.2) * const.hecto
-Temperatur = unc.ufloat(23.3, 0.1) + const.zero_Celsius
 
-def adiabtenkoeffizient_ruechhardt(m, V, r, n, t, p_0):
+def k_ruechhardt(m, V, r, n, t):
+	global p_0
 	T = t / n
-	p = p_0 + m*const.g / const.pi / r**2
+	print papstats.pformat(T, 'T')
+	p = p_0 + m * const.g / const.pi / r**2
 	k = 4 * m * V / r**4 / T**2 / p
 	return k
 
@@ -42,14 +59,18 @@ m = unc.ufloat(26.116, 0.002) * const.gram
 V = unc.ufloat(5370, 5) * const.centi**3
 r = 0.5 * unc.ufloat(15.95, 0.05) * const.milli
 n = unc.ufloat(100, 1) 
-t = unc.ufloat(1*60 + 37.26, 0.5)
+t = unc.ufloat(60 + 37.26, 0.5)
 
-print adiabtenkoeffizient_ruechhardt(m, V, r, n, t, p_0)
+k_L = k_ruechhardt(m, V, r, n, t)
+print papstats.pformat(k_L, label='k_L')
+papstats.print_rdiff(k_L, k_L_erw)
 
 m = unc.ufloat(26.006, 0.002) * const.gram
 V = unc.ufloat(5460, 5) * const.centi**3
 r = 0.5 * unc.ufloat(15.97, 0.05) * const.milli
 n = unc.ufloat(100, 1)
-t = unc.ufloat(1*60 + 33.42, 0.5)
+t = unc.ufloat(60 + 33.42, 0.5)
 
-print adiabtenkoeffizient_ruechhardt(m, V, r, n, t, p_0)
+k_Ar = k_ruechhardt(m, V, r, n, t)
+print papstats.pformat(k_Ar, label='k_Ar')
+papstats.print_rdiff(k_Ar, k_Ar_erw)
